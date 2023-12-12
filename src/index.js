868f2d8df8b7f0cc796c2654a1147bbf7b0c43c7
Mariadoc1 <153285337+Mariadoc1@users.noreply.github.com>
2023-12-12 19:14:46 +0000

    Used the API index to make the information real-time rather than made-up

--- src/index.js
+++ src/index.js
@@ -57,6 +57,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "7odb1604cd5806418921397fa0t4e3ac";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`;
@@ -64,26 +71,30 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  let forecastElement = document.querySelector("#forecast");
-
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">⛅️</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <div>
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+    </div>
     <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature-max">
-        <strong>15ºC</strong>
+        <strong>${Math.round(day.temperature.maximum)}º</strong>
       </div>
-      <div class="weather-forecast-temperature-min">9ºC</div>
+      <div class="weather-forecast-temperature-min">${Math.round(
+        day.temperature.minimum
+      )}º</div>
     </div>
   </div>`;
+    }
   });
 
+  let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
 
